19482e539adfd0ae1f644ae92bc903f4a4fc03f4
Muhydeen Adediran <adediranmuhydeen@yahoo.com>
2026-02-20 13:54:46 +0100

    todo mvc

--- SQ02/Car.cs
+++ SQ02/Car.cs
@@ -30,9 +30,44 @@ public class  Honda :Car
         var c = a + b + d;
         return c;
     }
+    List<int> myNumbers = new List<int>();
+    ICollection<int> myArray = new int[5];
+    Dictionary<string, int> myDictionary = new Dictionary<string, int>();
 }
 
 public interface IVehicle
 {
     void Drive();
 }
+
+// LINQ query in C#
+/*String and string method
+ * Int and int method
+ * Collection and collection method (List and Array)
+ * Generic Classes in C#
+ */
+
+public  class MyGenericClass
+{
+    List<Car> cars = new List<Car>
+    {
+        new Car { Make = "Honda", Model = "Civic", Year = 2020 },
+        new Car { Make = "Honda", Model = "CrossTour", Year = 2020 },
+        new Car { Make = "Honda", Model = "Accord", Year = 2020 },
+        new Car { Make = "Honda", Model = "Accord", Year = 2022 },
+        new Car { Make = "Toyota", Model = "Corolla", Year = 2019 },
+        new Car { Make = "Ford", Model = "Mustang", Year = 2022 },
+        new Car { Make = "KIA", Model = "Picanto", Year = 2022 },
+        new Car { Make = "Lexus", Model = "RX330", Year = 2020 },
+        new Car { Make = "Ford", Model = "Mustang", Year = 2023 },
+        new Car { Make = "Ford", Model = "Mustang", Year = 2021 },
+        new Car { Make = "Ford", Model = "Mustang", Year = 2022 },
+        new Car { Make = "Ford", Model = "Mustang", Year = 2021 },
+    };
+
+    
+}
+
+
+
+
--- WeekFour/Dog.cs
+++ WeekFour/Dog.cs
@@ -1,25 +1,25 @@
-﻿namespace WeekFour;
+﻿//namespace WeekFour;
 
-public class Dog : Animal
-{
-    public override void Speak()
-    {
-        Console.WriteLine($"Woof Woof, I have {NumberOfLegs} legs and my color is {Color}");
-    }
-}
+//public class Dog : Animal
+//{
+//    public override void Speak()
+//    {
+//        Console.WriteLine($"Woof Woof, I have {NumberOfLegs} legs and my color is {Color}");
+//    }
+//}
 
-public class Cat : Animal
-{
-    public override void Speak()
-    {
-        Console.WriteLine($"Meow Meow, I have {NumberOfLegs} legs and my color is {Color}");
-    }
-}
+//public class Cat : Animal
+//{
+//    public override void Speak()
+//    {
+//        Console.WriteLine($"Meow Meow, I have {NumberOfLegs} legs and my color is {Color}");
+//    }
+//}
 
-public class Lion : Animal
-{
-    public override void Speak()
-    {
-        Console.WriteLine($"Roar Roar, I have {NumberOfLegs} legs and my color is {Color}");
-    }
-}
+//public class Lion : Animal
+//{
+//    public override void Speak()
+//    {
+//        Console.WriteLine($"Roar Roar, I have {NumberOfLegs} legs and my color is {Color}");
+//    }
+//}
